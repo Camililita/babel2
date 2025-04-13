5029234b7329047488d3068892257c40d2ec791b
Algobvio <santiago1sanchezmoreno@gmail.com>
2025-04-13 15:35:39 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+import { Input } from "../components/ui/input";
 import { useEffect, useState } from "react";
 
 export default function LandingPage() {
