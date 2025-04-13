a75427676f8ddac0f12a16ee8ed64d7b351c402a
Algobvio <santiago1sanchezmoreno@gmail.com>
2025-04-13 15:03:37 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Babel</h1>
+      <p>La poesía está por empezar a respirar.</p>
+    </div>
+  );
+}
